e225bee49f3fd29b1b04c3b2231cff774f4fb9a1
Gagan Bindal <gaganbindal2002@gmail.com>
2023-05-23 02:09:05 +0530

    API Protection

--- src/container/Homepage/Homepage.js
+++ src/container/Homepage/Homepage.js
@@ -3,33 +3,38 @@ import "./Homepage.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import Row from "../../components/Row/Row"
-
+import requests from "../../requests/requests";
 
 function HomePage() {
     return (
         <div className="HomepageContainer"> 
             <Navbar showSignInButton={false} logOut={true} />
             <Banner />
-            <Row title ="Movie Trip HITS" 
-            fetchUrl = "https://api.themoviedb.org/3/trending/all/week?api_key=20e36c67eb6a93cdc35a8cff930c9893&language=en-US"
+            <Row title= "Trending NOW" 
+            fetchUrl={`https://api.themoviedb.org/3/${requests.fetchTrending}`}
             isLargeRow
             />
+            {/* <Row title ="NETFLIX Originals" 
+            fetchUrl = {`https://api.themoviedb.org/3/${requests.fetchNetflixOriginals}`}
+            isLargeRow
+            /> */}
             <Row title ="Action" 
-            fetchUrl= "https://api.themoviedb.org/3/discover/movie?api_key=20e36c67eb6a93cdc35a8cff930c9893&with_genres=28"
+            
+            fetchUrl= {`https://api.themoviedb.org/3/${requests.fetchActionMovies}`}
             isLargeRow
             
             />
             
             <Row title ="Horror" 
-            fetchUrl = "https://api.themoviedb.org/3/discover/movie?api_key=20e36c67eb6a93cdc35a8cff930c9893&with_genres=27"
+            fetchUrl = {`https://api.themoviedb.org/3/${requests.fetchHorrorMovies}`}
             isLargeRow
             />
             <Row title ="Comedy" 
-            fetchUrl = "https://api.themoviedb.org/3/discover/movie?api_key=20e36c67eb6a93cdc35a8cff930c9893&with_genres=35"
+            fetchUrl = {`https://api.themoviedb.org/3/${requests.fetchComedyMovies}`}
             isLargeRow
             />
             <Row title ="Documentaries" 
-            fetchUrl = "https://api.themoviedb.org/3/discover/movie?api_key=20e36c67eb6a93cdc35a8cff930c9893&with_genres=99"
+            fetchUrl = {`https://api.themoviedb.org/3/${requests.fetchDocumentaries}`}
             isLargeRow
             />
             
--- src/requests/requests.js
+++ src/requests/requests.js
@@ -1,9 +1,9 @@
 
-const API_KEY = "20e36c67eb6a93cdc35a8cff930c9893";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 const requests = {
-  // fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+   fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
@@ -12,6 +12,5 @@ const requests = {
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
   fetchtrailer: `&type=video&videoDefinition=high&key=${apiKey}`
 };
-//https://api.themoviedb.org/3/trending/all/week?api_key==20e36c67eb6a93cdc35a8cff930c9893&language=en-US
+
 export default requests;
-// https://image.tmdb.org/t/p/original/trending/all/week?api_key==20e36c67eb6a93cdc35a8cff930c9893&language=en-US
